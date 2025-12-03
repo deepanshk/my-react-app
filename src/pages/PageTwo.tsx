eef2cf1e109ccf1d5f291ed6bcf7b92109bb38e8
Deepansh Khurana <deepanshk00@gmail.com>
2025-12-03 19:11:02 +0530

    Add second embed on page 2

--- src/pages/PageTwo.tsx
+++ src/pages/PageTwo.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const PageTwo: React.FC = () => {
+  const sidedWidgetRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (sidedWidgetRef.current) {
+      sidedWidgetRef.current.setAttribute('clientid', '1');
+      sidedWidgetRef.current.setAttribute('placementid', '551');
+      sidedWidgetRef.current.setAttribute('embedfor', 'sequence');
+    }
+  }, []);
+
   return (
     <div>
       <h2>Page Two</h2>
       <p>This is the second demo page.</p>
+      <div
+        className="sided-widget"
+        ref={sidedWidgetRef}
+      ></div>
     </div>
   );
 };
