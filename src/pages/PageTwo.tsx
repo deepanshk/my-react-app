c81b02ee52e1b30c34c879bfab64c5a468226c6a
Deepansh Khurana <deepanshk00@gmail.com>
2025-12-05 11:33:59 +0530

    Fix attributes issue

--- src/pages/PageTwo.tsx
+++ src/pages/PageTwo.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-
 const PageTwo: React.FC = () => {
-  const sidedWidgetRef = React.useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    if (sidedWidgetRef.current) {
-      sidedWidgetRef.current.setAttribute('clientid', '1');
-      sidedWidgetRef.current.setAttribute('placementid', '551');
-      sidedWidgetRef.current.setAttribute('embedfor', 'sequence');
-    }
-  }, []);
 
   return (
     <div>
       <h2>Page Two</h2>
       <p>This is the second demo page.</p>
       <div
-        className="sided-widget"
-        ref={sidedWidgetRef}
+      className="sided-widget"
+      clientId="1"
+      placementId="551"
+      id="sided-widget"
       ></div>
     </div>
   );
